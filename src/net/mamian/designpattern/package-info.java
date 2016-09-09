9a0d7f19b8be3e4296230af9ebd120b5e37da1a9
mamian <mamianskyma@aliyun.com>
2016-09-10 00:24:00 +0800

    设计模式

---
+++ src/net/mamian/designpattern/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * 设计模式
+ *
+ * @author mamian
+ * @mail mamianskyma@aliyun.com
+ * @date 2016-8-18 11:33:21
+ * @copyright ©2016 马面 All Rights Reserved
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+package net.mamian.designpattern;
+
---
+++ src/net/mamian/designpattern/proxy/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 代理模式
+ *
+ * @author mamian
+ * @mail mamianskyma@aliyun.com
+ * @date 2016-9-10 01:04:42
+ * @copyright ©2016 马面 All Rights Reserved
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+package net.mamian.designpattern.proxy;
