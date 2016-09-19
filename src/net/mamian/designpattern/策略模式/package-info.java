760b695f6f57530397d44b53e650f132292cfd5b
mamian <mamianskyma@aliyun.com>
2016-09-19 22:29:29 +0800

    策略模式

---
+++ src/net/mamian/designpattern/策略模式/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 策略模式
+ *
+ * @author mamian
+ * @mail mamianskyma@aliyun.com
+ * @date 2016-9-19 01:04:42
+ * @copyright ©2016 马面 All Rights Reserved
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+package net.mamian.designpattern.代理模式;
