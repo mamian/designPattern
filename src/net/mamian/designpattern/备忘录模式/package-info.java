cc043e6ecfb6973286b4014a372012d9e6aff7f0
mamian <mamianskyma@aliyun.com>
2016-09-20 00:59:38 +0800

    备忘录模式

---
+++ src/net/mamian/designpattern/备忘录模式/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 备忘录模式
+ *
+ * @author mamian
+ * @mail mamianskyma@aliyun.com
+ * @date 2016-9-20 00:58:27
+ * @copyright ©2016 马面 All Rights Reserved
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+package net.mamian.designpattern.备忘录模式;
