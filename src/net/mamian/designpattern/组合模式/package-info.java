a14f9c912114d0262e067b3bcaff20bb5a1fcd94
mamian <mamianskyma@aliyun.com>
2016-09-22 22:49:45 +0800

    组合模式

---
+++ src/net/mamian/designpattern/组合模式/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 组合模式
+ *
+ * @author mamian
+ * @mail mamianskyma@aliyun.com
+ * @date 2016-9-22 22:46:07
+ * @copyright ©2016 马面 All Rights Reserved
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+package net.mamian.designpattern.组合模式;
