f6b7b083a6a7b50921730bfd33980e9199bd8741
mamian <mamianskyma@aliyun.com>
2016-09-25 20:56:38 +0800

    多例模式

---
+++ src/net/mamian/designpattern/多例模式/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 多例模式
+ *
+ * @author mamian
+ * @mail mamianskyma@aliyun.com
+ * @date 2016-9-25 20:55:21
+ * @copyright ©2016 马面 All Rights Reserved
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+package net.mamian.designpattern.多例模式;
