c86db586ebc040fb431cff4db4f43b843ad9c292
mamian <mamianskyma@aliyun.com>
2016-09-27 07:26:58 +0800

    责任链模式

---
+++ src/net/mamian/designpattern/责任链模式/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 责任链模式
+ *
+ * @author mamian
+ * @mail mamianskyma@aliyun.com
+ * @date 2016-9-27 07:26:45
+ * @copyright ©2016 马面 All Rights Reserved
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+package net.mamian.designpattern.责任链模式;
