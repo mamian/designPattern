2c2b96c279b7c558fa1e9076c3bd45940ea26534
mamian <mamianskyma@aliyun.com>
2016-09-26 23:48:55 +0800

    原型模式

---
+++ src/net/mamian/designpattern/原型模式/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 原型模式
+ *
+ * @author mamian
+ * @mail mamianskyma@aliyun.com
+ * @date 2016-9-26 23:48:28
+ * @copyright ©2016 马面 All Rights Reserved
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+package net.mamian.designpattern.原型模式;
