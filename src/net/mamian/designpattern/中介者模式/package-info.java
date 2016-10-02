203555827c0b410adbf190d20d9d7e8884b53ec5
mamian <mamianskyma@aliyun.com>
2016-10-02 21:14:48 +0800

    中介者模式

---
+++ src/net/mamian/designpattern/中介者模式/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 中介者模式
+ *
+ * @author mamian
+ * @mail mamianskyma@aliyun.com
+ * @date 2016-10-2 21:26:45
+ * @copyright ©2016 马面 All Rights Reserved
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+package net.mamian.designpattern.中介者模式;
