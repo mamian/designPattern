e42f84315c8810a98e0ebc441421b4b1ec5d1321
mamian <mamianskyma@aliyun.com>
2016-10-03 23:40:28 +0800

    状态模式

---
+++ src/net/mamian/designpattern/状态模式/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 状态模式
+ *
+ * @author mamian
+ * @mail mamianskyma@aliyun.com
+ * @date 2016-10-3 23:36:45
+ * @copyright ©2016 马面 All Rights Reserved
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+package net.mamian.designpattern.状态模式;
