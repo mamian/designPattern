85a9013dcddf4ad6a17dca2ed2addf0c9d9e1e8e
mamian <mamianskyma@aliyun.com>
2016-10-04 20:35:09 +0800

    装饰模式

---
+++ src/net/mamian/designpattern/装饰模式/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 装饰模式
+ *
+ * @author mamian
+ * @mail mamianskyma@aliyun.com
+ * @date 2016-10-4 20:34:32
+ * @copyright ©2016 马面 All Rights Reserved
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+package net.mamian.designpattern.装饰模式;
