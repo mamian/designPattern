047c1aa5c4a620813cda40a59adc5d7581fa6a37
mamian <mamianskyma@aliyun.com>
2016-10-06 19:45:13 +0800

    适配器模式

---
+++ src/net/mamian/designpattern/适配器模式/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 适配器模式
+ *
+ * @author mamian
+ * @mail mamianskyma@aliyun.com
+ * @date 2016-10-6 19:44:32
+ * @copyright ©2016 马面 All Rights Reserved
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+package net.mamian.designpattern.适配器模式;
