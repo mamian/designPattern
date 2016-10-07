8d5d2b5ade9a31171b9305983a5089e98e1b440c
mamian <mamianskyma@aliyun.com>
2016-10-07 21:27:08 +0800

    桥接模式

---
+++ src/net/mamian/designpattern/桥接模式/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 桥接模式
+ *
+ * @author mamian
+ * @mail mamianskyma@aliyun.com
+ * @date 2016-10-7 21:26:38
+ * @copyright ©2016 马面 All Rights Reserved
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+package net.mamian.designpattern.桥接模式;
