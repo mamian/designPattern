dfd6c65e4c4cadfa07faf9e4a9587067a2328d7d
mamian <mamianskyma@aliyun.com>
2016-10-10 22:01:14 +0800

    门面模式

---
+++ src/net/mamian/designpattern/门面模式/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 门面模式
+ *
+ * @author mamian
+ * @mail mamianskyma@aliyun.com
+ * @date 2016-10-10 22:00:38
+ * @copyright ©2016 马面 All Rights Reserved
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+package net.mamian.designpattern.门面模式;
