5dbdcd31fb330825e8126811c8ae552b6da9d99f
mamian <mamianskyma@aliyun.com>
2016-10-12 22:34:50 +0800

    解释器模式

---
+++ src/net/mamian/designpattern/解释器模式/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 解释器模式
+ *
+ * @author mamian
+ * @mail mamianskyma@aliyun.com
+ * @date 2016-10-12 22:34:38
+ * @copyright ©2016 马面 All Rights Reserved
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+package net.mamian.designpattern.解释器模式;
