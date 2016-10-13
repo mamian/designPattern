a858bbf11f32c1227f9616c568e560c6c4642116
mamian <mamianskyma@aliyun.com>
2016-10-13 21:59:10 +0800

    建造者模式

---
+++ src/net/mamian/designpattern/建造者模式/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 建造者模式
+ *
+ * @author mamian
+ * @mail mamianskyma@aliyun.com
+ * @date 2016-10-13 21:58:38
+ * @copyright ©2016 马面 All Rights Reserved
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+package net.mamian.designpattern.建造者模式;
