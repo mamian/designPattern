f618113a5fd6e7ea1b01286a9b1b7656122940f0
mamian <mamianskyma@aliyun.com>
2016-10-14 22:02:23 +0800

    简单工厂模式

---
+++ src/net/mamian/designpattern/简单工厂模式/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 简单工厂模式
+ *
+ * @author mamian
+ * @mail mamianskyma@aliyun.com
+ * @date 2016-10-14 22:01:38
+ * @copyright ©2016 马面 All Rights Reserved
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+package net.mamian.designpattern.简单工厂模式;
