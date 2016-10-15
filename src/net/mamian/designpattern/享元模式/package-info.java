431e7bb5e81c4e7ea7a9c9ad7b781312ae97244c
mamian <mamianskyma@aliyun.com>
2016-10-16 00:22:59 +0800

    享元模式

---
+++ src/net/mamian/designpattern/享元模式/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 享元模式
+ *
+ * @author mamian
+ * @mail mamianskyma@aliyun.com
+ * @date 2016-10-16 00:21:38
+ * @copyright ©2016 马面 All Rights Reserved
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+package net.mamian.designpattern.享元模式;
