ea116d159e36f39332b829d8f4e6e3132b977f03
mamian <mamianskyma@aliyun.com>
2016-10-18 23:48:37 +0800

    观察者模式

---
+++ src/net/mamian/designpattern/观察者模式/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 观察者模式
+ *
+ * @author mamian
+ * @mail mamianskyma@aliyun.com
+ * @date 2016-10-18 23:48:38
+ * @copyright ©2016 马面 All Rights Reserved
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+package net.mamian.designpattern.观察者模式;
