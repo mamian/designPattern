3d10e54baab63addd7afa98c2cc000a4ab474a8a
mamian <mamianskyma@aliyun.com>
2016-10-19 22:59:28 +0800

    工厂模式

---
+++ src/net/mamian/designpattern/工厂模式/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 工厂模式
+ *
+ * @author mamian
+ * @mail mamianskyma@aliyun.com
+ * @date 2016-10-19 22:59:38
+ * @copyright ©2016 马面 All Rights Reserved
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+package net.mamian.designpattern.工厂模式;
