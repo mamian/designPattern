05ebedf8a9255ac97d76a559b30adf0952df3403
mamian <mamianskyma@aliyun.com>
2016-10-20 23:06:46 +0800

    访问者模式

---
+++ src/net/mamian/designpattern/访问者模式/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 访问者模式
+ *
+ * @author mamian
+ * @mail mamianskyma@aliyun.com
+ * @date 2016-10-18 23:06:38
+ * @copyright ©2016 马面 All Rights Reserved
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+package net.mamian.designpattern.访问者模式;
