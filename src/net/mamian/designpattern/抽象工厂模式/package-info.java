bae5610e43189ba1c272b77b57fad35f37431435
mamian <mamianskyma@aliyun.com>
2016-10-22 22:02:05 +0800

    抽象工厂模式

---
+++ src/net/mamian/designpattern/抽象工厂模式/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 抽象工厂模式
+ *
+ * @author mamian
+ * @mail mamianskyma@aliyun.com
+ * @date 2016-10-22 22:01:20
+ * @copyright ©2016 马面 All Rights Reserved
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+package net.mamian.designpattern.抽象工厂模式;
