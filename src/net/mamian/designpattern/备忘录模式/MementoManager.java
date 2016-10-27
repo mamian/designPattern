a2fb064c47ea3620503300158f9cf5c57691d718
mamian <mamianskyma@aliyun.com>
2016-10-27 23:51:17 +0800

    备忘录模式

---
+++ src/net/mamian/designpattern/备忘录模式/MementoManager.java
@@ -0,0 +1,13 @@
+package net.mamian.designpattern.备忘录模式;
+
+public class MementoManager {
+	private Memento memento;
+
+	public Memento getMemento() {
+		return memento;
+	}
+
+	public void setMemento(Memento memento) {
+		this.memento = memento;
+	}
+}
