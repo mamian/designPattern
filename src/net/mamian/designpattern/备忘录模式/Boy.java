c35b957a119ea878411f7b1836bd1a82ec3c1ed2
mamian <mamianskyma@aliyun.com>
2016-12-16 22:42:04 +0800

    备忘录模式

---
+++ src/net/mamian/designpattern/备忘录模式/Boy.java
@@ -0,0 +1,42 @@
+package net.mamian.designpattern.备忘录模式;
+
+public class Boy {
+	private String name;
+	private String age;
+	private String sex;
+	
+	public Memento createMemento(){
+		return new Memento(name,age);
+	}
+	
+	public void backMemento(Memento memento){
+		this.name = memento.getMementoName();
+		this.age = memento.getMementoAge();
+	}
+	
+	public void getBoyInfo(){
+		System.out.println("姓名："+name+"，年龄："+age+"，性别："+sex);
+	}
+	
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+}
