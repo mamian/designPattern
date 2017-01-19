54ac6a341ff6f63e641a6e574501d871d7c2bc00
mamian <mamianskyma@aliyun.com>
2017-01-19 23:58:44 +0800

    add author

--- src/net/mamian/designpattern/中介者模式/Store.java
+++ src/net/mamian/designpattern/中介者模式/Store.java
@@ -1,5 +1,14 @@
 package net.mamian.designpattern.中介者模式;
 
+/**
+ *
+ *
+ * @author mamian
+ * @mail mamianskyma@aliyun.com
+ * @date 2017-1-19 23:57:41
+ * @copyright ?2016 马面 All Rights Reserved
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
 public class Store extends AbstractColleague {
     public Store(AbstractMediator mediator) {
         super(mediator);
