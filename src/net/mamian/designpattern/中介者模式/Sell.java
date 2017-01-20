5cfba1a87ff07492a1d3059660a753d18c403442
马面 <mamian@users.noreply.github.com>
2017-01-21 00:06:52 +0800

    Update Sell.java

--- src/net/mamian/designpattern/中介者模式/Sell.java
+++ src/net/mamian/designpattern/中介者模式/Sell.java
@@ -1,1 +1,20 @@
-package net.mamian.designpattern.中介者模式;/** *  *  * @author mamian * @mail mamianskyma@aliyun.com * @date 2016-10-2 21:26:45 * @copyright ©2016 马面 All Rights Reserved * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER. */public class Sell extends AbstractColleague {    public Sell(AbstractMediator mediator) {        super(mediator);    }    public void startSell(int num) {        super.mediator.execute("销售", num);        System.out.println("Sell对象执行startSell方法");    }}
+package net.mamian.designpattern.中介者模式;
+
+/**
+ * 
+ * @author mamian
+ * @mail mamianskyma@aliyun.com
+ * @date 2016-10-2 21:26:45
+ * @copyright ©2016 马面 All Rights Reserved
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+public class Sell extends AbstractColleague {
+    public Sell(AbstractMediator mediator) {
+        super(mediator);
+    }
+
+    public void startSell(int num) {
+        super.mediator.execute("销售", num);
+        System.out.println("Sell对象执行startSell方法");
+    }
+}
