25db13714b1a2846a8a42b74de836946fdce7afb
mamian <mamianskyma@aliyun.com>
2017-01-28 12:28:51 +0800

    抽象工厂

--- src/net/mamian/designpattern/抽象工厂模式/Client.java
+++ src/net/mamian/designpattern/抽象工厂模式/Client.java
@@ -1,7 +1,14 @@
 package net.mamian.designpattern.抽象工厂模式;
 
+/**
+ * @author mamian
+ * @mail mamianskyma@aliyun.com
+ * @date 2017-01-29 19:21:25
+ * @copyright ©2017 马面 All Rights Reserved
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
 public class Client {
-	public static void main(String[] args){
-		
-	}
+    public static void main(String[] args) {
+
+    }
 }
