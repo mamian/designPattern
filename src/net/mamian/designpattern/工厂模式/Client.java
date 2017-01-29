aef58196153a95410ed101d4cf0a3f186bf12a5c
mamian <mamianskyma@aliyun.com>
2017-01-29 21:13:49 +0800

    工厂模式

--- src/net/mamian/designpattern/工厂模式/Client.java
+++ src/net/mamian/designpattern/工厂模式/Client.java
@@ -1,10 +1,16 @@
 package net.mamian.designpattern.工厂模式;
 
-/*
- * 
- * */
+/**
+ * 工厂模式
+ *
+ * @author mamian
+ * @mail mamianskyma@aliyun.com
+ * @date 2017-01-27 19:59:20
+ * @copyright ©2017 马面 All Rights Reserved
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
 public class Client {
-	public static void main(String[] args) {
-		
-	}
+    public static void main(String[] args) {
+
+    }
 }
