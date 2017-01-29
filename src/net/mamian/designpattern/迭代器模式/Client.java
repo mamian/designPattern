e345534d9f87fdbbd386d78bfe9251d8e427df63
mamian <mamianskyma@aliyun.com>
2017-01-29 20:56:18 +0800

    迭代器模式

--- src/net/mamian/designpattern/迭代器模式/Client.java
+++ src/net/mamian/designpattern/迭代器模式/Client.java
@@ -1,8 +1,14 @@
 package net.mamian.designpattern.迭代器模式;
 
-/*
+/**
  * 直接使用iterator()即可
- * */
+ *
+ * @author mamian
+ * @mail mamianskyma@aliyun.com
+ * @date 2016-10-18 20:55:23
+ * @copyright ©2016 马面 All Rights Reserved
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
 public class Client {
 	public static void main(String[] args) {
 		
